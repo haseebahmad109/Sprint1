66650da05092d0301de3283ad9a152979eeda1a2
haseebahmad109 <haseebahmad109@gmail.com>
2013-09-06 13:18:45 -0700

    =error box in screen 1

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,14 @@ $(document).ready(function(){
 	    $("menu a").css("background-color", "");
 	});
 
+	$("submit-join-form-main-page").click(function () {
+	    if ("errors are present") {
+	        $(".joinIn-box-bottom .form-labels label").css("line-height","4.2");
+	        return false;
+	    } else
+	        return true;
+	});
+
 
 });
 
