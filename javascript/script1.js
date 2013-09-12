34f3cf87be0ebda60761ae84745d11c8361e4c0e
haseebahmad109 <haseebahmad109@gmail.com>
2013-09-12 13:06:35 -0700

    =improved screen reset password and added jquery removed javascript

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,12 +1,16 @@
-﻿function validateEmail() {
-    var e = document.getElementById("emailtoget");
-    var email = e.value;
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(email)) {
-       
-        var t = document.getElementById("hideme");
-        t.style.display = "block";
+﻿$(document).ready(function () {
+    $("#submit-reset-request").click(function () {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        if ($("#emailtoget").val() == "" || !emailReg.test($("#emailtoget").val())) {
+            $("#error-box").removeClass("hidden");
+            $("#error-box div p").html("<b>Please correct the marked field(s) below.</b>");
+            if ($("#emailtoget").val() == "")
+                $("#emailErrors").html("Please enter a value.");
+            else $("#emailErrors").html("Please enter a valid email address.");
+            return false;
         }
+        else return true;
+    });
+});
 
-}
 
